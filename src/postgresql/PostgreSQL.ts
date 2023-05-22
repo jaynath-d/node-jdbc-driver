2fa1aff66b4fed1a9805eee042d60d1f7583cb81
Jaynath Ray <126139404+jaynath-d@users.noreply.github.com>
2023-05-22 21:31:46 +0530

    Update PostgreSQL.ts

--- src/postgresql/PostgreSQL.ts
+++ src/postgresql/PostgreSQL.ts
@@ -1,5 +1,5 @@
 
-import IConnectionType from '../IConnectionType';
+import IConnectionType, { QueryType } from '../IConnectionType';
 import IConnectionConfig from '../IConnectionConfig';
 
 export default class PostgreSQL implements IConnectionType{
@@ -17,6 +17,15 @@ export default class PostgreSQL implements IConnectionType{
             drivername: this.driverName,
             user: this.config.username,
             password: this.config.password
+        }
+    }
+
+    public get_query = (tableName:string, type:string = 'D') => {
+        if(QueryType.columns == type){
+            return `SELECT column_name as col_name, data_type FROM information_schema.columns WHERE table_name = '${tableName}'`
+        }else{
+            // QueryType.describe
+            return `SELECT count(*) as total_rows, pg_size_pretty( pg_total_relation_size('${tableName}') ) as total_size;`
         }
     }
 
