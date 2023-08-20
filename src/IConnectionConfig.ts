47ab30a7c55667c672ba9aa8ab26e594a46be983
Jaynath Ray <126139404+jaynath-d@users.noreply.github.com>
2023-08-20 15:49:09 +0530

    Update IConnectionConfig.ts

--- src/IConnectionConfig.ts
+++ src/IConnectionConfig.ts
@@ -1,8 +1,19 @@
-// Created Connection Config interface
-export default interface IConnectionConfig {
+interface OptionalConfig{
+    minpoolsize?: number;
+    maxpoolsize?: number;
+}
+export interface BaseConfig extends OptionalConfig{
     host: string;
     port: number;
     database: string;
     username: string;
     password: string;
 }
+
+export interface SQLiteConfig extends OptionalConfig{
+    path: string;
+}
+
+type IConnectionConfig<T> = T extends 'S' ? SQLiteConfig : BaseConfig 
+
+export default IConnectionConfig
