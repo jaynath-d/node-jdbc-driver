5e7f3532307287d9de3a73c62bedb4ee5e1df5b1
Jaynath Ray <126139404+jaynath-d@users.noreply.github.com>
2023-08-23 14:56:11 +0530

    Update IConnectionConfig.ts

--- src/IConnectionConfig.ts
+++ src/IConnectionConfig.ts
@@ -1,19 +1,48 @@
+type MakeNever<T> = {
+    [K in keyof T]?: never;
+};
+
 interface OptionalConfig{
     minpoolsize?: number;
     maxpoolsize?: number;
 }
-export interface BaseConfig extends OptionalConfig{
+
+interface AuthConfig extends OptionalConfig{
+    username?: string;
+    password?: string;
+}
+
+interface JDBCConfig{
+    jdbcUrl: string;
+}
+
+interface HostConfig extends AuthConfig{
     host: string;
     port: number;
     database: string;
-    username: string;
-    password: string;
 }
+
+interface PathConfig extends OptionalConfig{
+    path: string;
+}
+
+type H = MakeNever<JDBCConfig> & HostConfig
+type J = MakeNever<HostConfig> & JDBCConfig
+
+export type BaseConfig = H | J
 
 export interface SQLiteConfig extends OptionalConfig{
     path: string;
 }
 
-type IConnectionConfig<T> = T extends 'S' ? SQLiteConfig : BaseConfig 
+export interface CustomDriverConfig extends Partial<HostConfig>, Partial<SQLiteConfig>, Partial<JDBCConfig>{
+    jars: string,
+    driverClass: string
+};
+
+type IConnectionConfig<T> =
+    T extends 'S' ? SQLiteConfig :
+    T extends 'C' ? CustomDriverConfig :
+    BaseConfig; 
 
 export default IConnectionConfig
